ab2466be4bd55121a5954fa8f2459b431f3f126f
Tahim Kader <tahimkader@Tahims-MacBook-Pro.local>
2017-08-13 00:58:41 -0700

    updated weatherlist component

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 
 
-export default class WeatherList extends Component {
+class WeatherList extends Component {
+
+  //so state.weather is an array of weather objects.
+  //taking one of the weather objects in the array (through the map function) to pass into renderWeather
+  //cityData is the object that has all the weather info for that one city in the array
+  renderWeather(cityData){
+    return (
+        <tr key ={cityData.city.name}>
+          <td>{cityData.city.name}</td>
+        </tr>
+    );
+  }
+
   render(){
     return (
       <table className = "table table-hover">
         <thead>
           <tr>
             <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
           </tr>
         </thead>
         <tbody>
-
+            {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     )
   }
 }
+
+//weather key is named that same as the root reducer weather
+function mapStateToProps(state){
+  return { weather: state.weather};
+}
+
+//need to get data/ interact weatherlist component with redux
+//so now we have access to weather data through props
+export default connect(mapStateToProps)(WeatherList);
