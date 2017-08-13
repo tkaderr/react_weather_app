b2a0728d8ee70ea4bded8ab065d3b5fd5bcfb7d4
Tahim Kader <tahimkader@Tahims-MacBook-Pro.local>
2017-08-12 23:25:39 -0700

    Practice with react and redux

---
+++ src/actions/index.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const API_KEY= "b1413b70bc1fd163763e0acbae76c378";
+//those are back ticks not quotes
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`  ;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+//actioncreators always return an actions
+//action is an object that always have a type
+//actioncreator will have argument city passed
+export function fetchWeather(city){
+
+  const url = `${ROOT_URL}&q=${city},us`;
+
+  //axios is like ajax request
+  //returns a promise
+  const request = axios.get(url);
+
+  return ({
+    type: FETCH_WEATHER,
+    //payload contains data that describes this particular action
+    payload: request
+  });
+}
---
+++ src/containers/search_bar.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchWeather} from '../actions/index';
+
+class SearchBar extends Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = {term: ''};
+
+    //bind onInputChange to this(Searchbar) and replace onInputChange with the bound instance
+    //you need to bind context whenever in your jsx has a callback/function
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  //all dom handlers they all come along with this event object always
+  onInputChange(event){
+    console.log(event.target.value);
+    //whenever we update the input, we want to update the state
+    //if you use "this" it doesnt refer to the component but something else
+    this.setState({term: event.target.value});
+  }
+
+  onFormSubmit(event){
+    //dont submit form
+    event.preventDefault();
+    this.props.fetchWeather(this.state.term);
+    //clear the input field after
+    this.setState({term: ''});
+  }
+
+  render(){
+    return (
+      <form onSubmit = {this.onFormSubmit} className = "input-group">
+        <input
+          placeholder="Get a five day forecast in your favorite cities"
+          className = "form-control"
+          value = {this.state.term}
+          onChange = {this.onInputChange}
+        />
+        <span className = "input-group-btn">
+            <button type="submit" className = "btn btn-secondary">Submit</button>
+        </span>
+      </form>
+    );
+  }
+}
+
+//hook up the SearchBar to the fetchWeather actioncreator
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({fetchWeather}, dispatch);
+}
+
+//null because we dont need state here
+export default connect(null, mapDispatchToProps)(SearchBar);
